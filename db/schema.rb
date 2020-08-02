b6f9a83ce05a3c82a887fc93731afc388fe28375
Amirhossein Akbari <amirhosseinakbarie@gmail.com>
2020-08-02 16:27:31 +0430

    rename password to password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_080242) do
+ActiveRecord::Schema.define(version: 2020_08_02_115440) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_080242) do
     t.date "birth_date"
     t.string "email"
     t.string "phone"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
