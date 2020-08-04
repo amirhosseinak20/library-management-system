9bedbf68f56c89ec4755659f6c35a6bc9fbb719f
Amirhossein Akbari <amirhosseinakbarie@gmail.com>
2020-08-04 12:48:37 +0430

    landing page added

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,8 @@
 class LandingController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
+    @books = Book.order('created_at desc').limit(4)
+    @authors = User.joins(:role).where(roles: { name: 'author' }).order('updated_at desc').limit(4)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/create'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/show'
+  get 'books/update'
+  get 'books/destroy'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
       registrations: 'users/registrations',
@@ -8,6 +16,6 @@ Rails.application.routes.draw do
   }
   root 'landing#index'
   get 'landing/index'
-  resources :users
+  resources :users, :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
