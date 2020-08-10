478a567f61bed7f3445de6823d9600cf5d6d2a46
Amirhossein Akbari <amirhosseinakbarie@gmail.com>
2020-08-10 17:03:09 +0430

    refactor landing controller

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,13 +1,8 @@
 class LandingController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-  # before_action :init_user
+  skip_before_action :authenticate_user!
 
   def index
-    # @books = Book.order('created_at desc').limit(4)
-    # @authors = User.joins(:role).where(roles: { name: 'author' }).order('updated_at desc').limit(4)
-  end
-
-  def init_user
-    @current_user = current_user || User.new
+    @books = Book.order('created_at desc').limit(4)
+    @authors = User.joins(:role).where(roles: { name: 'author' }).order('updated_at desc').limit(4)
   end
 end
